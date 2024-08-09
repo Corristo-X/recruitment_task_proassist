5ca10171dabda7f6b914a7e294e75eaeef6f4c5e
Daniel Talarek <daniel@soffee.pl>
2024-08-09 17:11:52 +0200

    Add SecurityController and configure security.yml for authentication

---
+++ proassist/src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/api/login", name="api_login")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+    }
+}
